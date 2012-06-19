a2d7f6bf82eb29c5bff669a84c71d2886d7bd166
Paul Schütte <schuette.paul@gmail.com>
2012-06-20 00:59:27 +0200

    Timing and feedback
    Added timing to the sketch
    Made the debug-LED blink in-time
    Use the state of the Motor to determine it's state

--- mimosa_lab_control.ino
+++ mimosa_lab_control.ino
@@ -6,43 +6,84 @@
 #define downtime 19800000 // 1/2 hour in milliseconds
 
 MS_DCMotor motor(MOTOR_A);
-//int led = 13;
+int led = 13;
 int closedSwitch = 2;
 int openedSwitch = 4;
+int motorRunning = BRAKE;
+long long sketchtime = 0;
+long long  sleeptime = uptime;
+long long waketime = downtime;
 
 void setup() {
-  motor.setSpeed(0);
-  motor.run(BRAKE);
+  brakeMotor();
+  pinMode(led, OUTPUT);
   pinMode(closedSwitch, INPUT);
   pinMode(openedSwitch, INPUT);
 }
 
 void loop() {
-  // wind up the box
-  // wait for half an hour
-  // wind down the box
-  // wait for 5.5 hours
+  sketchtime = millis();
+  switch (motor.getState()) {
+  case BRAKE:
+    if (waketime <= sketchtime) {
+      wakeUp();
+    }
+    if (sleeptime <= sketchtime) {
+      gotoSleep();
+    }
+    break;
+  case FORWARD:
+    if (digitalRead(openedSwitch) == HIGH) {
+      brakeMotor();
+    }
+    break;
+  case BACKWARD:
+    if (digitalRead(closedSwitch) == HIGH) {
+      brakeMotor();
+    }
+    break;
+  default:
+    if ((digitalRead(closedSwitch) == LOW) && 
+      (digitalRead(openedSwitch) == LOW)) {
+      // wind up the box initially
+      windUp();
+      wakeUp();
+    }
+    break;
+  }
 }
 
-// run the motor until the end-switch is hit
+void wakeUp() {
+  sleeptime = millis() + uptime;
+  waketime = sleeptime + downtime;
+  // TODO:
+  // set off all the other switches
+  digitalWrite(led, HIGH);
+}
+
+void gotoSleep() {
+  waketime = millis() + downtime;
+  sleeptime = waketime + uptime;
+  // TODO:
+  // set off all the other switches
+  digitalWrite(led, LOW);
+}
+
 void windUp() {
+  motor.run(RELEASE);
   motor.setSpeed(255);
   motor.run(FORWARD);
-  while (digitalRead(openedSwitch) != 1) {
-    motor.run(RELEASE);
-  }
+}
+
+void windDown() {
+  motor.run(RELEASE);
+  motor.setSpeed(255);
+  motor.run(BACKWARD);
+}
+
+void brakeMotor() {
   motor.setSpeed(0);
   motor.run(BRAKE);
 }
 
-// run the motor until the end-switch is hit
-void windDown() {
-    motor.setSpeed(255);
-  motor.run(BACKWARD);
-  while (digitalRead(closedSwitch) != 1) {
-    motor.run(RELEASE);
-  }
-  motor.setSpeed(0);
-  motor.run(BRAKE);
-}
 
